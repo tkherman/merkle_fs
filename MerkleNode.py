6f6489750859b6d5e33c12ef190d1b1956e3f6e9
John McGuinness <johnmcguinness@Johns-MacBook-Pro.local>
2018-11-15 17:48:12 -0600

    working on function to find file from mfs by name

--- MerkleNode.py
+++ MerkleNode.py
@@ -3,7 +3,7 @@ import sys
 import boto3
 from botocore.exceptions import ClientError
 
-dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+dynamodb = boto3.resource('dynamodb', region_name='us-east-2')
 
 class MerkleNode:
     def __init__(self):
@@ -45,10 +45,46 @@ def fetch_node(fs, cksum):
     mNode.prev_version = item.get('prev_version', None)
     mNode.next_version = item.get('next_version', None)
     mNode.is_dir = item.get('is_dir', None)
+	mNode.parent_node = item.get('parent_node', None)
     mNode.s3_ref = item.get('s3_ref', None)
-    mNode.dir_info = item.get('s3_ref', None)
+    mNode.dir_info = item.get('dir_info', None)
     mNode.mod_time = item.get('mod_time', None)
     mNode.mod_user = item.get('mod_user', None)
 
 
     return mNode
+
+def fetchNodeByName(fs):
+	root_table = dynamodb.Table('root_pointers')
+	try:
+		response = root_table.get_item(
+			Key={
+				'name': fs
+			}
+		)
+	except ClientError as e:
+		return None
+	
+	root_item = response.get('Item')
+	
+	if not root_item:
+		return None
+	
+	root_ptr = item.get('name', None)
+
+    fs_table = dynamodb.Table(fs)
+    try:
+        response = fs_table.get_item(
+            Key={
+                'cksum': cksum
+            }
+        )
+    except ClientError as e:
+        return None
+    item = response.get('Item')
+
+    if not item:
+        return None
+	
+	#TODO - traverse fs_table filesystem to find the correct node
+	
