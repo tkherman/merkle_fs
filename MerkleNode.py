5476399adcabf42439db1394d14dc9f1aac05115
John McGuinness <johnmcguinness@Johns-MBP.fios-router.home>
2018-11-21 00:01:45 -0500

    fixed cksum collision bug in put

--- MerkleNode.py
+++ MerkleNode.py
@@ -1,109 +1,122 @@
 from __future__ import print_function
 import sys
 import boto3
+import getpass
 import hashlib
+import datetime
 from botocore.exceptions import ClientError
 
 dynamodb = boto3.resource('dynamodb', region_name='us-east-2')
 
 class MerkleNode:
-    def __init__(self):
-        self.cksum = None
-        self.name = None
-        self.is_dir = None
-        self.dir_info = None
-        self.mod_time = None
-        self.mod_user = None
+	def __init__(self):
+		self.cksum = None
+		self.name = None
+		self.is_dir = None
+		self.dir_info = None
+		self.mod_time = None
+		self.mod_user = None
 
-    def print_info(self):
-        print("cksum:       {}".format(self.cksum))
-        print("name:        {}".format(self.name))
-        print("is_dir:      {}".format(self.is_dir))
-        print("dir_info:    {}".format(self.dir_info))
-        print("mod_time:    {}".format(self.mod_time))
-        print("mod_user:    {}".format(self.mod_user))
+	def print_info(self):
+		print("cksum:		{}".format(self.cksum))
+		print("name:		{}".format(self.name))
+		print("is_dir:		{}".format(self.is_dir))
+		print("dir_info:	{}".format(self.dir_info))
+		print("mod_time:	{}".format(self.mod_time))
+		print("mod_user:	{}".format(self.mod_user))
 
 # Return None if cannot fetch node from table
 def fetch_node(fs, cksum):
-    fs_table = dynamodb.Table(fs)
+	fs_table = dynamodb.Table(fs)
 
-    try:
-        response = fs_table.get_item(
-            Key={
-                'cksum': cksum
-            }
-        )
-    except ClientError as e:
-        return None
+	try:
+		response = fs_table.get_item(
+			Key={
+				'cksum': cksum
+			}
+		)
+	except ClientError as e:
+		return None
 
-    item = response.get('Item')
+	item = response.get('Item')
 
-    if not item:
-        return None
+	if not item:
+		return None
 
-    # Create MerkleNode object
-    mNode = MerkleNode()
-    mNode.cksum = cksum
-    mNode.name = item.get('name', None)
-    mNode.is_dir = item.get('is_dir', None)
-    mNode.dir_info = item.get('dir_info', None)
-    mNode.mod_time = item.get('mod_time', None)
-    mNode.mod_user = item.get('mod_user', None)
+	# Create MerkleNode object
+	mNode = MerkleNode()
+	mNode.cksum = cksum
+	mNode.name = item.get('name', None)
+	mNode.is_dir = item.get('is_dir', None)
+	mNode.dir_info = item.get('dir_info', None)
+	mNode.mod_time = item.get('mod_time', None)
+	mNode.mod_user = item.get('mod_user', None)
 
 
-    return mNode
+	return mNode
 
 
 # Insert the new node into DB
 def insert_node(fs, mNode):
-    fs_table = dynamodb.Table(fs)
+	fs_table = dynamodb.Table(fs)
 
-    try:
-        response = fs_table.put_item(
-            Item={
-                'cksum': mNode.cksum,
-                'name': mNode.name,
-                'is_dir': mNode.is_dir,
-                'dir_info': mNode.dir_info,
-                'mod_time': mNode.mod_time,
-                'mod_user': mNode.mod_user
-            }
-        )
-    except ClientError as e:
-        return False
+	try:
+		response = fs_table.put_item(
+			Item={
+				'cksum': mNode.cksum,
+				'name': mNode.name,
+				'is_dir': mNode.is_dir,
+				'dir_info': mNode.dir_info,
+				'mod_time': mNode.mod_time,
+				'mod_user': mNode.mod_user
+			}
+		)
+	except ClientError as e:
+		return False
 
-    return True
+	return True
+
+# calculate cksum of new file
+def calculate_cksum(src_filepath, dest_filepath):
+	hasher = hashlib.sha256()
+	hasher.update(dest_filepath)
+	hasher.update("********") #TODO - eventually make this a random hash
+	with open(src_filepath, "rb") as f:
+		for chunk in iter(lambda: f.read(4096), b""):
+			hasher.update(chunk)
+
+	return hasher.hexdigest()
 
 # Take dir_info list and return cksum of directory
 def calculate_dir_cksum(dir_info):
-    hasher = hashlib.sha256()
-    hasher.update(dir_info[0])
+	hasher = hashlib.sha256()
+	hasher.update(dir_info[0])
 
-    if len(dir_info) == 1:
-        return hasher.hexdigest()
+	if len(dir_info) == 1:
+		return hasher.hexdigest()
 
-    for sub_info in dir_info[1:]:
-        hasher.update(sub_info[0])
-        hasher.update(sub_info[1])
+	for sub_info in dir_info[1:]:
+		hasher.update(sub_info[0])
+		hasher.update(sub_info[1])
 
-    return hasher.hexdigest()
+	return hasher.hexdigest()
 
 
 def get_merkle_node_by_name(fs, curr_node, path_list, nodes_traversed=None):
-    if nodes_traversed:
-        nodes_traversed.append(curr_node)
+	if nodes_traversed:
+		nodes_traversed.append(curr_node)
+	
+	if not curr_node.is_dir:
+		return None
 
-    if not curr_node.is_dir:
-        return None
+	for sub_f in curr_node.dir_info[1:]:
+		if sub_f[0] == path_list[0]:
+			if len(path_list) == 1:
+				return fetch_node(fs, sub_f[1])
+			return get_merkle_node_by_name(fs, fetch_node(fs, sub_f[1]), path_list[1:])
 
-    for sub_f in curr_node.dir_info[1:]:
-        if sub_f[0] == path_list[0]:
-            if len(path_list) == 1:
-                return fetch_node(fs, sub_f[1])
-            return get_merkle_node_by_name(fs, fetch_node(fs, sub_f[1]), path_list[1:])
-
-    # Incorrect path
-    return None
+	# Incorrect path
+	return None
 
 def fetch_fs_root_node(fs):
 	root_ptrs_table = dynamodb.Table('root_pointers')
@@ -125,3 +138,35 @@ def fetch_fs_root_node(fs):
 		retmsg = e.response['Error']['Message']
 
 	return success, retmsg
+	
+# Bubble up and create new node for all ancestors
+def bubble_up(fs, newNode, nodes_traversed):
+	curr_fname = newNode.name
+	curr_cksum = newNode.cksum
+	for ancestor_node in reversed(nodes_traversed):
+		new_aNode = MerkleNode()
+		new_aNode.name = ancestor_node.name
+		new_aNode.is_dir = ancestor_node.is_dir
+
+		# Generate new dir_info
+		new_dir_info = ancestor_node.dir_info
+		existing_node_found = False
+		if not len(new_dir_info) == 1: # there are sub files/directories inside
+			for sub_f in new_dir_info:
+				if sub_f[0] == curr_fname:
+					sub_f[1] = curr_cksum
+					existing_node_found = True
+					break
+		if not existing_node_found:
+			new_dir_info.append([curr_fname, curr_cksum])
+		new_aNode.dir_info = new_dir_info
+		new_aNode.cksum = calculate_dir_cksum(new_dir_info)
+		new_aNode.mod_time = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
+		new_aNode.mod_user = getpass.getuser()
+
+		insert_node(fs, new_aNode)
+
+		curr_fname = new_aNode.name
+		curr_cksum = new_aNode.cksum
+		print('End', curr_fname)
+	return curr_cksum
--- put.py
+++ put.py
@@ -5,8 +5,7 @@ import datetime
 import os.path
 import getpass
 
-from MerkleNode import MerkleNode, fetch_node, get_merkle_node_by_name, insert_node, calculate_dir_cksum, fetch_fs_root_node
-from utilities import *
+from MerkleNode import MerkleNode, fetch_node, get_merkle_node_by_name, insert_node, calculate_cksum, calculate_dir_cksum, fetch_fs_root_node, bubble_up
 
 dynamodb = boto3.resource('dynamodb', region_name='us-east-2')
 s3 = boto3.resource('s3')
@@ -21,34 +20,36 @@ def PUT(fs, src_filepath, dest_filepath):
 		s3_bucket, root_cksum = msg
 	else:
 		print('Error: {}'.format(msg))
-		exit(1)
+		return("unsuccessful")
 
 	# Get node of directory the file is to be placed in
 	nodes_traversed = []
 	root_node = fetch_node(fs, root_cksum)
-	dest_filepath = dest_filepath.lstrip('/').split('/')
-	if len(dest_filepath) > 1:
-		dirpath = dest_filepath[:-1]
+	dest_filepath_list = dest_filepath.lstrip('/').split('/')
+	if len(dest_filepath_list) > 1:
+		dirpath = dest_filepath_list[:-1]
 		dir_node = get_merkle_node_by_name(fs, root_node, dirpath, nodes_traversed)
 	else:
 		dir_node = root_node
 		nodes_traversed.append(root_node)
 
-	# Check if file already exist
+	# Check if file already exist -- TODO - unsure of purpose here
 	original_fnode = None
 	for sub_f in dir_node.dir_info[1:]:
-		if sub_f[0] == dest_filepath[-1]:
+		if sub_f[0] == dest_filepath_list[-1]:
 			original_fnode = fetch_node(fs, sub_f[1])
+	#TODO - do something with this information
 
 	# Create MerkleNode object for new node
 	newNode = MerkleNode()
-	newNode.cksum = calculate_cksum(src_filepath)
-	newNode.name = dest_filepath[-1]
+	newNode.cksum = calculate_cksum(src_filepath, dest_filepath)
+	newNode.name = dest_filepath_list[-1]
 	newNode.is_dir = False
 	newNode.dir_info = None
 	newNode.mod_time = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
 	newNode.mod_user = getpass.getuser()
 
+	# Insert to DB
 	if not insert_node(fs, newNode):
 		return "Failed to update DB"
 
@@ -56,36 +57,9 @@ def PUT(fs, src_filepath, dest_filepath):
 	s3.meta.client.upload_file(src_filepath, s3_bucket, newNode.cksum)
 
 	# Bubble up and create new node for all ancestors
-	curr_fname = newNode.name
-	curr_cksum = newNode.cksum
-	for ancestor_node in reversed(nodes_traversed):
-		new_aNode = MerkleNode()
-		new_aNode.name = ancestor_node.name
-		new_aNode.is_dir = ancestor_node.is_dir
+	curr_cksum = bubble_up(fs, newNode, nodes_traversed)
 
-		# Generate new dir_info
-		new_dir_info = ancestor_node.dir_info
-		existing_node_found = False
-		if not len(new_dir_info) == 1: # there are sub files/directories inside
-			for sub_f in new_dir_info:
-				if sub_f[0] == curr_fname:
-					sub_f[1] = curr_cksum
-					existing_node_found = True
-					break
-		if not existing_node_found:
-			new_dir_info.append([curr_fname, curr_cksum])
-		new_aNode.dir_info = new_dir_info
-		new_aNode.cksum = calculate_dir_cksum(new_dir_info)
-		new_aNode.mod_time = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
-		new_aNode.mod_user = getpass.getuser()
-
-		new_aNode.print_info()
-		insert_node(fs, new_aNode)
-
-		curr_fname = new_aNode.name
-		curr_cksum = new_aNode.cksum
-
-	# curr_fname and curr_cksum should contain root / cksum,
+	# curr_fname and curr_cksum should contain root / cksum, -- TODO ???
 	# Update root_pointers table
 	root_pointers_table = dynamodb.Table('root_pointers')
 	response = root_pointers_table.update_item(
@@ -101,4 +75,4 @@ def PUT(fs, src_filepath, dest_filepath):
 	return "successful"
 
 
-print(PUT("dev", "go_irish.txt", "/goirish5.txt"))
+print(PUT("dev", "go_irish.txt", "/goirish2.txt"))
