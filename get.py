5c26dcfacf787d0ccdbb8e954a03d29d34ea5543
tkherman <ktong1@nd.edu>
2018-12-04 14:15:26 -0500

    caching added

--- get.py
+++ get.py
@@ -1,6 +1,9 @@
 from __future__ import print_function
-import boto3
 from botocore.exceptions import ClientError
+from shutil import copyfile
+import boto3
+import os
+import tempfile
 
 from MerkleNode import MerkleNode, fetch_node, get_merkle_node_by_name
 
@@ -33,6 +36,22 @@ def GET(fs, src_path, dest_path):
 	if not file_node:
 		return "File {} does not exist".format(src_path)
 
-	# Get file from s3
-	s3.meta.client.download_file(s3_bucket, file_node.cksum, dest_path)
+	### Get file from s3 or from cache
+	# Check if the file is in cache
+	cache_dir = tempfile.gettempdir() + "/merkle_fs_data_blocks"
+	if not os.path.isdir(cache_dir):
+		try:
+			os.mkdir(cache_dir)
+		except:
+			print("Unable to create cache directory")
+
+	cache_block_path = "{}/{}".format(cache_dir, file_node.cksum)
+	if not os.path.exists(cache_block_path):
+		s3.meta.client.download_file(s3_bucket, file_node.cksum, cache_block_path)
+
+	try:
+		copyfile(cache_block_path, dest_path)
+	except:
+		print("Unable to get file from {}".format(cache_dir))
+
 	return "Downloaded {} to {}".format(src_path, dest_path)
